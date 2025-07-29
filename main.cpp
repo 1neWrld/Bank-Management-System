70e67e0370065e458af7e86fec84a0e4719a44ee
1neWrld <wandipamarema@icloud.com>
2025-07-29 15:00:36 +0200

    Added simple main menu in main.cpp file. Will be updated, in future commits

--- main.cpp
+++ main.cpp
@@ -1,27 +1,80 @@
 #include <iostream>
 
-#include "BankAccount.cpp"
+#include "BankAccount.h"
 
 
 int main()
 {
 
-    BankAccount account("John Doe", "123456789", "1234");
+    using namespace std;
 
-    account.deposit(1000.0);
-    std::cout << "Balance after deposit: " << account.getBalance() << std::endl;
+    std::string name;
+    std::string accountNumber;
+    std::string pin;
 
-    if (account.withdraw(500.0)) {
-        std::cout << "Withdrawal successful. New balance: " << account.getBalance() << std::endl;
-    } else {
-        std::cout << "Withdrawal failed." << std::endl;
-    }
+    cout << "---------------------------" << endl;
+    cout << "Welcome to the 1neWrld Bank!" << endl;
+    cout << "---------------------------" << endl;
 
-    if (account.VerifyPin("124")) {
-        std::cout << "PIN verified successfully." << std::endl;
-    } else {
-        std::cout << "PIN verification failed." << std::endl;
-    }
+    cout << "Please enter your Full Name." << endl;
+    std::getline(cin, name);
+    std::cout << "Enter your desired account number: ";
+    std::cin >> accountNumber;
+    std::cout << "Set a 4-digit PIN: ";
+    std::cin >> pin;
+
+    BankAccount account(name, accountNumber, pin);
+
+    cout << "Your Wrld has been created successfully!" << endl;
+
+    int option;
+    double amount;
+    
+    do {
+        std::cout << "\n1. Deposit\n2. Withdraw\n3. Check Balance\n4. Exit\nSelect: ";
+        std::cin >> option;
+        
+        switch (option)
+        {
+        case 1:
+            std::cout << "Enter amount to deposit: ";
+            std::cin >> amount;
+            account.deposit(amount);
+            
+            break;
+
+        case 2:
+            std::cout << "Enter amount to withdraw: ";
+            std::cin >> amount;
+            account.withdraw(amount);
+
+            break;
+
+        case 3:
+            std::cout << "Current balance: " << account.getBalance() << std::endl;
+            
+            break;
+
+        case 4:
+            std::cout << "Exiting the program." << std::endl;
+            
+            break;
+
+        default:
+             std::cout << "Invalid option. Please try again." << std::endl;
+            
+             break;
+        }
+
+
+    } while (option != 4);
+
+        cout << "----------------------------------" << endl;
+    std::cout << "Thank you for using 1neWrld Bank!" << std::endl;
+        cout << "----------------------------------" << endl;
+        
+    return 0;
+
 
     return 0;
 
